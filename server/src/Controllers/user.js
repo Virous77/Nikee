68d42b908e65897043221aabeb5cb5528c52532c
Reetesh Kumar <rajreetesh7@gmail.com>
2023-04-29 10:43:09 +0530

    created api for authentication

--- server/src/Controllers/user.js
+++ server/src/Controllers/user.js
@@ -6,9 +6,7 @@ export const createUser = async (req, res, next) => {
   const { name, email, image, about, password } = req.body;
 
   if (!name || !email || !password) {
-    return next(
-      createError({ status: 400, message: "Fields can't be empty!" })
-    );
+    return next(createError({ status: 400, message: "Fields can't be empty" }));
   }
 
   const salt = await bcrypt.genSalt(10);
@@ -29,5 +27,82 @@ export const createUser = async (req, res, next) => {
   try {
   } catch (error) {
     next(next);
+  }
+};
+
+export const loginUser = async (req, res, next) => {
+  const { email, password: userPass } = req.body;
+  if (!email || !userPass) {
+    return next(createError({ status: 400, message: "Fields can't be empty" }));
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return next(createError({ status: 400, message: "User not exists" }));
+  }
+
+  const pass = await bcrypt.compare(userPass, user.password);
+  if (!pass) {
+    return next(createError({ status: 400, message: "Password is incorrect" }));
+  }
+
+  const { password, ...otherDetails } = user._doc;
+  res.status(200).json(otherDetails);
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateUser = async (req, res, next) => {
+  const id = req.params.id;
+  const { name, image, about } = req.body;
+
+  const user = await User.findById(id);
+  if (!user) {
+    return next(createError({ status: 400, message: "User not exists" }));
+  }
+
+  const { name: userName, image: userImage, about: userAbout } = user;
+  user.name = name || userName;
+  user.image = image || userImage;
+  user.about = about || userAbout;
+
+  try {
+    const updatedUser = await user.save();
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const changePassword = async (req, res, next) => {
+  const id = req.params.id;
+  const { password, newPassword } = req.body;
+  if (!password || !newPassword) {
+    return next(createError({ status: 400, message: "Fields can't be empty" }));
+  }
+
+  const user = await User.findById(id);
+  if (!user) {
+    return next(createError({ status: 400, message: "User not exists" }));
+  }
+
+  const pass = await bcrypt.compare(password, user.password);
+  if (!pass) {
+    return next(
+      createError({ status: 400, message: "Old password not match" })
+    );
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(newPassword, salt);
+
+  user.password = hash;
+  await user.save();
+  res.status(200).json({ message: "Password changed successfully" });
+  try {
+  } catch (error) {
+    next(error);
   }
 };
--- server/src/Routes/user.js
+++ server/src/Routes/user.js
@@ -1,8 +1,16 @@
 import express from "express";
-import { createUser } from "../Controllers/user.js";
+import {
+  changePassword,
+  createUser,
+  loginUser,
+  updateUser,
+} from "../Controllers/user.js";
 
 const router = express.Router();
 
 router.post("/user", createUser);
+router.get("/user", loginUser);
+router.put("/user/:id", updateUser);
+router.put("/user/change-pass/:id", changePassword);
 
 export default router;
