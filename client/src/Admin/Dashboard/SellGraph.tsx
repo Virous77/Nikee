fb1a773dd21b117e11efdae76d3f257ee47ba40b
Reetesh Kumar <rajreetesh7@gmail.com>
2023-06-02 00:57:25 +0530

    added chart for admin dashboard

--- client/src/Admin/Dashboard/SellGraph.tsx
+++ client/src/Admin/Dashboard/SellGraph.tsx
@@ -1,8 +1,42 @@
+import React, { useMemo } from "react";
 import styles from "./Dashboard.module.scss";
 import { useQuery } from "react-query";
 import { getData } from "../../api/api";
 import { useGlobalContext } from "../../store/GlobalContext";
 import { AppError, Order } from "../../interfaces/interface";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+import { month } from "../../utils/data";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options: any = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top",
+    },
+    title: {
+      display: true,
+      text: "Monthly Sell",
+    },
+  },
+};
 
 const SellGraph = () => {
   const { handleSetNotification } = useGlobalContext();
@@ -24,7 +58,82 @@ const SellGraph = () => {
     }
   );
 
-  return <div className={styles.orders}>SellGraph</div>;
+  const today = new Date();
+  const thirtyDaysAgo = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() - 30
+  );
+
+  const lastThirtyDaysData = orders?.filter(
+    (item) => new Date(item.createdAt) >= thirtyDaysAgo
+  );
+
+  const makeData = lastThirtyDaysData?.map((order) => {
+    const data = { amount: order.amount, date: order.createdAt };
+    return data;
+  });
+
+  const date = useMemo(() => {
+    const data: string[] | undefined = makeData?.map((subs) => {
+      const makeDate = new Date(subs.date);
+      const day = makeDate.getDate();
+      const currentMonth = makeDate.getMonth();
+      return `${day} ${month[currentMonth]}`;
+    });
+
+    return data;
+  }, [makeData]);
+
+  const uniqueDate = [...new Set(date)];
+
+  const priceByDate = useMemo(() => {
+    const data =
+      lastThirtyDaysData &&
+      lastThirtyDaysData.reduce((acc, curr) => {
+        const date = `${new Date(curr.createdAt).getDate()} ${new Date(
+          curr.createdAt
+        ).getMonth()}`;
+
+        const amount = curr.amount;
+
+        if (acc.has(date)) {
+          acc.set(date, acc.get(date) + amount);
+        } else {
+          acc.set(date, amount);
+        }
+
+        return acc;
+      }, new Map());
+
+    return (
+      data &&
+      Array.from(data, ([date, price]) => ({
+        [date]: price,
+      })).flatMap((price) => Object.values(price))
+    );
+  }, [orders]);
+
+  const data = {
+    labels: uniqueDate,
+    datasets: [
+      {
+        label: "Last 30 days sell",
+        data: priceByDate,
+        backgroundColor: "black",
+      },
+    ],
+  };
+
+  return (
+    <React.Fragment>
+      {date && date?.length > 0 && (
+        <main className={styles["daily-chart"]}>
+          <Bar options={options} data={data} />
+        </main>
+      )}
+    </React.Fragment>
+  );
 };
 
 export default SellGraph;
